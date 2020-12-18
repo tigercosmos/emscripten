70eb5dc72a168051f88c070274eb1fc6593ac661
tigercosmos <phy.tiger@gmail.com>
2020-12-18 18:34:02 +0800

    let self.location.href empty

--- src/shell.js
+++ src/shell.js
@@ -118,7 +118,9 @@ var _scriptDir = import.meta.url;
 var _scriptDir = (typeof document !== 'undefined' && document.currentScript) ? document.currentScript.src : undefined;
 
 if (ENVIRONMENT_IS_WORKER) {
-  _scriptDir = self.location.href;
+  // OFFWORKER
+  // _scriptDir = self.location.href;
+  _scriptDir = "";
 }
 #if ENVIRONMENT_MAY_BE_NODE
 else if (ENVIRONMENT_IS_NODE) {
@@ -289,7 +291,10 @@ if (ENVIRONMENT_IS_SHELL) {
 #if ENVIRONMENT_MAY_BE_WEB || ENVIRONMENT_MAY_BE_WORKER
 if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
   if (ENVIRONMENT_IS_WORKER) { // Check worker, not web, since window could be polyfilled
-    scriptDirectory = self.location.href;
+    // OffWORKER
+
+    // scriptDirectory = self.location.href;
+    scriptDirectory = "";
   } else if (typeof document !== 'undefined' && document.currentScript) { // web
     scriptDirectory = document.currentScript.src;
   }
